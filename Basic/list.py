d0abded94acc200a498cabaa0a03c534c3510793
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-06-24 23:33:23 +0530

    list tuple

---
+++ Basic/list.py
@@ -0,0 +1,18 @@
+stud = [656 , " abcd " , 98.98 , True]
+
+len(stud)
+
+stud[0] = 616
+
+stud[ 1:3]
+
+print(stud)
+print(type(stud))
+
+stud.sort()
+stud.sort(reverse=True)
+stud.reverse()
+stud.append()
+stud.remove()
+# stud.pop(inx)
+# stud.insert(inx ,el)
---
+++ Basic/tuple.py
@@ -0,0 +1,7 @@
+tup = (86 , 87 , 98 , 998)
+
+tup1 = (1,)
+print(type(tup1))
+
+tup.index(86)
+tup.count(98)
