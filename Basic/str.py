9ce7e3491ccc84bf358474fc85cd7c70651dd5c0
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-06-24 16:16:35 +0530

    Py

---
+++ Basic/str.py
@@ -0,0 +1,12 @@
+str1 = "jbsjdbb'svahv "
+str2 = 'sdkncn mncj " jcjsb'
+
+print(str1+str2 , len(str1))
+
+print(str2[1:4])
+
+str.endswith()
+str.capitalize()
+str.replace()
+str.find()
+str.count()
