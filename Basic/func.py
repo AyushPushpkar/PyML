5bd2ba6bdd19e159b206e5e830f3accae7bd9557
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-06-25 23:08:11 +0530

    func

---
+++ Basic/func.py
@@ -0,0 +1,24 @@
+
+# Immutable Types (like int, str) = Acts like PBV
+def modify_number(n):
+    print("Inside function before change:", n)
+    n = n + 5
+    print("Inside function after change:", n)
+
+num = 10
+
+modify_number(num)
+print("Outside function:", num)
+
+
+# Mutable Types (like list, dict) = Acts like PBR
+def add_item(mylist):
+    print("Before:", mylist)
+    mylist.append(100)
+    print("After:", mylist)
+
+numbers = [1, 2, 3]
+add_item(numbers)
+print("Outside function:", numbers)
+
+print(add_item(numbers))
