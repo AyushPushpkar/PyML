6865ba402960b5bbda542e105058f06b030b30ad
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2025-07-03 00:22:41 +0530

    data analysis

---
+++ Pandas/dataanalysis.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+try:
+    transaction_df = pd.read_csv("C:/Users/ayush/Jupyter/transaction_data.csv") 
+    customer_df = pd.read_csv("C:/Users/ayush/Jupyter/customers.csv")
+
+    transaction_df.head()
+    customer_df.head()
+    transaction_df.info()
+    customer_df.info()
+
+    transaction_df['Date'] = pd.to_datetime(transaction_df['Date'])
+    customer_df['CustomerID'].value_counts().sum()
+
+    transaction_df['CustomerID'].nunique() # how many purchased
+
+    merged_df = pd.merge(transaction_df , customer_df , on = 'CustomerID' , how = 'inner')
+
+    age_summary = merged_df.groupby('Age')['Quantity'].sum() # groupby age and sum the related quantity
+    country_summary = merged_df.groupby('Country').agg({'Quantity' : 'sum' , 'Total' : 'sum'})
+
+    country_summary = merged_df.groupby('Country').agg({
+        'Quantity': ['sum', 'mean', 'max', 'min'],
+        'Total': ['sum', 'mean', 'std'],
+        'CustomerID': 'nunique'
+    })
+
+    country_summary['Total'].plot(kind= 'bar' ,figsize = (8,4) , title = 'Total sales by country')
+
+    country_summary.plot(
+        kind='pie',
+        y='Total',
+        autopct='%1.1f%%',
+        figsize=(8, 8),
+        legend=False,
+        title='Sales Distribution by Country',
+        subplots=True
+    )
+
+    plt.ylabel('')
+    plt.tight_layout()
+    plt.show()
+except Exception as e:
+    print("❌ Error :", e)
